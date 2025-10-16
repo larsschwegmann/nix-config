5e2816be5cbf734f04ff6989e337f87dd9bb6c19
Lars Schwegmann <schwegmannlars@gmail.com>
2025-10-16 18:55:24 +0200

    dd kamino http ingress host

---
+++ hosts/kamino-http-ingress/configuration.nix
@@ -0,0 +1,47 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
+  nix.settings = { sandbox = false; };  
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = true;
+  };
+
+  networking.hostName = "kamino-http-ingress";
+  networking.hostId = "66ac1322";
+
+  # security.pam.services.sshd.allowNullPassword = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+        PermitRootLogin = "prohibit-password";
+        PasswordAuthentication = false;
+    };
+  };
+
+  users.users."root".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTxrW9jRI2GrxpnAFUfOgz79+exH4zOQYV+Qw9Ge5MM lars@mandalore"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    nano
+    wget
+    git
+    htop
+    iperf3
+  ];
+
+
+  system.stateVersion = "25.05";
+}
+
